48b049a9763a9cfabac69a677f03244aebe8410c
raimeiga <yusuke612@hotmail.com>
2023-09-17 09:38:30 +0900

    12.3 データの検索機能を作成

--- read.php
+++ read.php
@@ -13,14 +13,31 @@
     } else {
        $order = NULL;
     }
-
-    if ($order === 'desc') {
-        $sql_select = 'SELECT * FROM products ORDER BY updated_at DESC';
+    
+    // keywordパラメータの値が存在すれば（商品名を検索したとき）、その値を変数$keywordに代入する    
+    if (isset($_GET['keyword'])) {
+        $keyword = $_GET['keyword'];
     } else {
-        $sql_select = 'SELECT * FROM products ORDER BY updated_at ASC';
+        $keyword = NULL;
     }
+    
+    // orderパラメータの値によってSQL文を変更する  
+    if ($order === 'desc') {
+        $sql_select = 'SELECT * FROM products WHERE product_name LIKE :keyword ORDER BY updated_at DESC';    } else {
+        $sql_select = 'SELECT * FROM products WHERE product_name LIKE :keyword ORDER BY updated_at ASC';    
+        }
+     // SQL文を用意する
+     $stmt_select = $pdo->prepare($sql_select);
+
+     // SQLのLIKE句で使うため、変数$keyword（検索ワード）の前後を%で囲む（部分一致）
+     // 補足：partial match＝部分一致
+     $partial_match = "%{$keyword}%";
+ 
+     // bindValue()メソッドを使って実際の値をプレースホルダにバインドする（割り当てる）
+     $stmt_select->bindValue(':keyword', $partial_match, PDO::PARAM_STR);
+ 
      // SQL文を実行する
-     $stmt_select = $pdo->query($sql_select);
+     $stmt_select->execute();
  
      // SQL文の実行結果を配列で取得する
      $products = $stmt_select->fetchAll(PDO::FETCH_ASSOC);
@@ -60,12 +77,18 @@
              <div class="products-ui">
                  <div>
                  <a href="read.php?order=desc">
-                 <img src="images/desc.png" alt="降順に並び替え" class="sort-img">
-             </a>
-             <a href="read.php?order=asc">
-                 <img src="images/asc.png" alt="昇順に並び替え" class="sort-img">
-             </a>
+                   <img src="images/desc.png" alt="降順に並び替え" class="sort-img">
+                 </a>
+                 <a href="read.php?order=asc">
+                   <img src="images/asc.png" alt="昇順に並び替え" class="sort-img">
+                 </a>
+                 <!-- getとpostの役割りを書いとけ   -->
+                 <form action="read.php" method="get" class="search-form">
+                   <input type="text" class="search-box" placeholder="商品名で検索" name="keyword" value="<?= $keyword ?>">
+                 
+                </form> 
                  </div>
+
                  <a href="#" class="btn">商品登録</a>
              </div>
              <table class="products-table">
